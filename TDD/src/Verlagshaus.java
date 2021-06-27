71a4642cc1c86169e059da09f00b86d7a9acf3cc
Dirk Drutschmann <dirkdrutschmann@gmail.com>
2021-06-27 20:56:07 +0200

    update

---
+++ TDD/src/Verlagshaus.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+public class Verlagshaus {
+
+	
+	public double textToEuro(String text){
+		if(text == null) {
+			throw new IllegalArgumentException("String can't be null!");
+		}
+	    return Math.round(100*(double)text.length()/1000.0)/100.0;
+	}
+	
+	public int countPictures(String text) {
+		text = text.toLowerCase();
+		Matcher m = Pattern.compile("(?=(picture))").matcher(text);
+		List<Integer> pos = new ArrayList<Integer>();
+		while (m.find())
+		{
+		    pos.add(m.start());
+		}
+		return pos.size();
+	}
+	
+}
